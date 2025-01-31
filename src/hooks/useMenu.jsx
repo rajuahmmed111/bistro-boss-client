a621f20c24c659167b4e64237e0a781ece0e246e
Raju Ahmmed <ahraju2003@gmail.com>
2025-01-31 11:15:47 +0600

    load menu data

--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -3,7 +3,7 @@ import { useEffect, useState } from "react";
 const useMenu = () => {
   const [menu, setMenu] = useState([]);
 
-  const [loading, setLoading] = useState(true)
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("http://localhost:5000/menu")
