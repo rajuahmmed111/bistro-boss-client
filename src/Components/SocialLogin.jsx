cae817b332735179b3b447c75a0e268ae7d9f06b
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-02 11:04:39 +0600

    update google login

--- src/Components/SocialLogin.jsx
+++ src/Components/SocialLogin.jsx
@@ -1,11 +1,28 @@
 import { FaGoogle } from "react-icons/fa";
+import useAuth from "../hooks/useAuth";
 
 const SocialLogin = () => {
+  const { signInWithGoogle } = useAuth();
+
+  const handleGoogleSignIn = async () => {
+    await signInWithGoogle()
+      .then((res) => {
+        const user = res.user;
+        console.log("User signed in google:", user);
+      })
+      .catch((error) => {
+        console.log("Error signing in with google:", error);
+      });
+  };
+
   return (
     <div className="mx-8 mb-5">
-        <div className="divider"></div>
+      <div className="divider"></div>
       <div>
-        <button className="btn w-full text-[#D1A054] hover:text-white bg-white border-2 border-[#D1A054] hover:bg-[#D1A054]">
+        <button
+          onClick={handleGoogleSignIn}
+          className="btn w-full text-[#D1A054] hover:text-white bg-white border-2 border-[#D1A054] hover:bg-[#D1A054]"
+        >
           <FaGoogle />
           Google
         </button>
