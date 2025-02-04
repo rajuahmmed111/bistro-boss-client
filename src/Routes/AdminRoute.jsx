2bd37af3505d2fb8b80384c261efbb41a2507658
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-04 15:53:37 +0600

    update admin route

--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -2,22 +2,20 @@ import { Navigate, useLocation } from "react-router-dom";
 import useAdmin from "../hooks/useAdmin";
 import useAuth from "../hooks/useAuth";
 
-const AdminRoute = ({children}) => {
-  const [user, loading] = useAuth();
+const AdminRoute = ({ children }) => {
+  const {user, loading} = useAuth();
   const [isAdmin, isAdminLoading] = useAdmin();
   const location = useLocation();
-
-  if (loading || isAdminLoading) {
-    return (
-      <>
-        <span className="loading loading-spinner text-warning"></span>
-        <span className="loading loading-spinner text-error"></span>
-      </>
-    );
-  }
-
   if (user || isAdmin) {
     return children;
+  }
+  if (loading || isAdminLoading) {
+    return (
+      <div>
+        <span className="loading loading-spinner text-warning"></span>
+        <span className="loading loading-spinner text-error"></span>
+      </div>
+    );
   }
   return <Navigate to="/login" state={{ from: location }} replace></Navigate>;
 };
