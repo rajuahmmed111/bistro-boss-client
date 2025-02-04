c292ba6441940376ce4d7c81acb7be5e0fb41cec
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-04 22:22:03 +0600

    add manage item delete

--- src/Pages/Dashboard/ManageItems/ManageItems.jsx
+++ src/Pages/Dashboard/ManageItems/ManageItems.jsx
@@ -1,7 +1,40 @@
-import { FaTrashAlt } from "react-icons/fa";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import useMenu from "../../../hooks/useMenu";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
+import Swal from "sweetalert2";
 
 const ManageItems = () => {
+  const [menu, refetch] = useMenu();
+  const axiosSecure = useAxiosSecure();
+
+  const handleDeleteItem = (item) => {
+    // delete item from database
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!",
+    }).then((result) => {
+      if (result.isConfirmed) {
+        axiosSecure.delete(`/menu/${item._id}`).then((res) => {
+          if (res.data.deletedCount > 0) {
+            refetch();
+
+            Swal.fire({
+              title: "Deleted!",
+              text: "Your file has been deleted.",
+              icon: "success",
+            });
+          }
+        });
+      }
+    });
+  };
+
   return (
     <div>
       <SectionTitle
@@ -10,7 +43,7 @@ const ManageItems = () => {
       ></SectionTitle>
       <div className="bg-white p-5">
         <h2 className="text-3xl font-semibold font-serif mt-4">
-          Total Users : {users.length}
+          Total ITEMS : {menu.length}
         </h2>
 
         {/* table */}
@@ -20,24 +53,40 @@ const ManageItems = () => {
             <thead className="bg-[#D1A054] text-white text-[16px] font-semibold p-5">
               <tr>
                 <th>#</th>
-                <th>NAME</th>
-                <th>EMAIL</th>
-                <th>ROLE</th>
+                <th>ITEM IMAGE</th>
+                <th>ITEM NAME</th>
+                <th>PRICE</th>
+                <th>ACTION</th>
                 <th>ACTION</th>
               </tr>
             </thead>
 
             <tbody>
               {/* row  */}
-              {users.map((user, idx) => (
-                <tr key={user._id}>
+              {menu.map((item, idx) => (
+                <tr key={item._id}>
                   <td>{idx + 1}</td>
-                  <td>{user.name}</td>
-                  <td>{user.email}</td>
+                  <td>
+                    <div className="avatar">
+                      <div className="mask mask-squircle h-12 w-12">
+                        <img
+                          src={item.image}
+                          alt="Avatar Tailwind CSS Component"
+                        />
+                      </div>
+                    </div>
+                  </td>
+                  <td>{item.name}</td>
+                  <td>{item.price}</td>
 
                   <th>
+                    <button className="btn bg-[#D1A054] text-white btn-sm">
+                      <FaEdit />
+                    </button>
+                  </th>
+                  <th>
                     <button
-                      onClick={() => handleDelete(user._id)}
+                      onClick={() => handleDeleteItem(item)}
                       className="btn bg-[#B91C1c] text-white btn-sm"
                     >
                       <FaTrashAlt />
--- src/hooks/useMenu.jsx
+++ src/hooks/useMenu.jsx
@@ -1,20 +1,35 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
+import useAxiosPublic from "./useAxiosPublic";
+import { useQuery } from "@tanstack/react-query";
 
 const useMenu = () => {
-  const [menu, setMenu] = useState([]);
+  // const [menu, setMenu] = useState([]);
 
-  const [loading, setLoading] = useState(true);
+  // const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    fetch("http://localhost:5000/menu")
-      .then((res) => res.json())
-      .then((data) => {
-        setMenu(data);
-        setLoading(false);
-      });
+  // useEffect(() => {
+  //   fetch("http://localhost:5000/menu")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setMenu(data);
+  //       setLoading(false);
+  //     });
+  // });
+
+  const axiosPublic = useAxiosPublic();
+  const {
+    data: menu = [],
+    isPending: loading,
+    refetch,
+  } = useQuery({
+    queryKey: ["menu"],
+    queryFn: async () => {
+      const res = await axiosPublic.get("/menu");
+      return res.data;
+    }, // A custom server-side function to fetch the data
   });
 
-  return [menu, loading];
+  return [menu, loading, refetch];
 };
 
 export default useMenu;
