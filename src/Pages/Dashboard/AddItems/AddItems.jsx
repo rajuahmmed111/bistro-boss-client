8110326a51f31d41f0ef29c1fbfdcb7f671a7808
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-04 20:04:46 +0600

    in dashboard Add items component

--- src/Pages/Dashboard/AddItems/AddItems.jsx
+++ src/Pages/Dashboard/AddItems/AddItems.jsx
@@ -1,9 +1,100 @@
-
+import { useForm } from "react-hook-form";
+import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
+import { FaUtensils } from "react-icons/fa";
 
 const AddItems = () => {
-  return (
-    <div>AddItems</div>
-  )
-}
+  const { register, handleSubmit } = useForm();
 
-export default AddItems
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
+  return (
+    <div>
+      <SectionTitle
+        subHeading="What's new?"
+        heading="ADD AN ITEM"
+      ></SectionTitle>
+
+      {/* react hooks from */}
+      <div>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          {/* recipe name */}
+          <div className="form-control w-full my-6">
+            <label className="label">
+              <span className="label-text">Recipe Name*</span>
+            </label>
+            <input
+              type="text"
+              {...register("name", { required: true })}
+              placeholder="Recipe Name"
+              className="input input-bordered w-full"
+            />
+          </div>
+
+          <div className="flex gap-6">
+            {/* category */}
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Category*</span>
+              </label>
+              <select
+                defaultValue="default"
+                {...register("category", { required: true })}
+                className="select select-bordered w-full"
+              >
+                <option disabled value="default">
+                  Select a category
+                </option>
+                <option value="salad">Salad</option>
+                <option value="pizza">Pizza</option>
+                <option value="soup">Soup</option>
+                <option value="dessert">Dessert</option>
+                <option value="drinks">Drinks</option>
+              </select>
+            </div>
+            {/* price */}
+            <div className="form-control w-full">
+              <label className="label">
+                <span className="label-text">Price*</span>
+              </label>
+              <input
+                type="number"
+                placeholder="Price"
+                {...register("price", { required: true })}
+                className="input input-bordered w-full"
+              />
+            </div>
+          </div>
+
+          {/* recipe details */}
+          <div className="form-control mt-4">
+            <label className="label">
+              <span className="label-text">Recipe Details</span>
+            </label>
+            <textarea
+              {...register("recipe")}
+              className="textarea textarea-bordered h-24"
+              placeholder="Recipe Details"
+            ></textarea>
+          </div>
+
+          {/* upload images */}
+          <div className="form-control w-full my-6">
+            <input
+              {...register("image", { required: true })}
+              type="file"
+              className="file-input w-full"
+            />
+          </div>
+          {/* submit button */}
+          <button type="submit" className="btn bg-gradient-to-r from-[#876924] to-[#B38030] text-white">
+            Add Items <FaUtensils className="ml-1 " />
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default AddItems;
--- src/Routes/AdminRoute.jsx
+++ src/Routes/AdminRoute.jsx
@@ -2,7 +2,7 @@ import { Navigate, useLocation } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import useAdmin from "../hooks/useAdmin";
 
-const AdminRoute = (children) => {
+const AdminRoute = ({children}) => {
   const { user, loading } = useAuth();
   const [isAdmin, isAdminLoading] = useAdmin();
   const location = useLocation();
