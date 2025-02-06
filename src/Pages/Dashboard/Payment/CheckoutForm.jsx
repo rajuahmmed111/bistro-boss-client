6637a778e288e5731104b564c60723075ffc4307
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-06 18:14:46 +0600

    checkout last update

--- src/Pages/Dashboard/Payment/CheckoutForm.jsx
+++ src/Pages/Dashboard/Payment/CheckoutForm.jsx
@@ -4,6 +4,7 @@ import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "../../../hooks/useCart";
 import useAuth from "../../../hooks/useAuth";
 import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
 
 const CheckoutForm = () => {
   const [error, setError] = useState("");
@@ -14,10 +15,12 @@ const CheckoutForm = () => {
   const elements = useElements();
 
   const axiosSecure = useAxiosSecure();
-  const {  user } = useAuth();
+  const { user } = useAuth();
 
   const [cart, refetch] = useCart();
   const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+  const navigate = useNavigate();
 
   useEffect(() => {
     if (totalPrice > 0) {
@@ -98,6 +101,7 @@ const CheckoutForm = () => {
             showConfirmButton: false,
             timer: 1500,
           });
+          navigate("/dashboard/payment-history");
         }
       }
     }
