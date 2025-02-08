0e4d6441fa4a7e5f558b95a04a7cafe8f2e00e08
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-08 11:02:45 +0600

    update pie chart

--- src/Pages/Dashboard/HomeAdmin/HomeAdmin.jsx
+++ src/Pages/Dashboard/HomeAdmin/HomeAdmin.jsx
@@ -68,7 +68,6 @@ const HomeAdmin = () => {
     innerRadius,
     outerRadius,
     percent,
-    index,
   }) => {
     const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
     const x = cx + radius * Math.cos(-midAngle * RADIAN);
