455ab8a036bdcd096f10a3236e96880ec68da7cf
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-08 12:30:03 +0600

    last update

--- src/Pages/Home/Testimonial/Testimonial.jsx
+++ src/Pages/Home/Testimonial/Testimonial.jsx
@@ -11,7 +11,7 @@ const Testimonial = () => {
   const [reviews, setReviews] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:5000/reviews")
+    fetch("https://bistro-boss-server-six-teal.vercel.app/reviews")
       .then((res) => res.json())
       .then((data) => setReviews(data));
   });
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -144,7 +144,7 @@ export const router = createBrowserRouter([
           </AdminRoute>
         ),
         loader: ({ params }) =>
-          fetch(`http://localhost:5000/menu/${params.id}`),
+          fetch(`https://bistro-boss-server-six-teal.vercel.app/menu/${params.id}`),
       },
       {
         path: "users",
