8b4eee5ec51830d3f1e260f03b0cc9381b2aa6d3
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-12 20:49:28 +0600

    added the to custom hooks and payment info which send to database for save

--- src/Pages/Dashboard/Payment/SslPayment.jsx
+++ src/Pages/Dashboard/Payment/SslPayment.jsx
@@ -4,6 +4,18 @@ import useCart from "../../../hooks/useCart";
 const SslPayment = () => {
   const { user } = useAuth();
   const { cart } = useCart();
+  const totalPrice = cart.reduce((total, item) => total + item.price, 0);
+
+  const payment = {
+    email: user?.email,
+    amount: totalPrice,
+    transactionId: "",
+    date: new Date(), // convert utc time use moment js
+    cartIds: cart.map((item) => item._id),
+    menuItemIds: cart.map((item) => item.menuId),
+    status: "pending",
+  };
+
   return <div>SslPayment</div>;
 };
 
