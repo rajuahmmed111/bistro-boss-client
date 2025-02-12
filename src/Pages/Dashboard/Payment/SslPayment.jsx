91a3df772ef4314855c717eca6dbfc917cb9fabc
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-12 21:07:31 +0600

    create a post requesst

--- src/Pages/Dashboard/Payment/SslPayment.jsx
+++ src/Pages/Dashboard/Payment/SslPayment.jsx
@@ -1,9 +1,15 @@
+import Swal from "sweetalert2";
 import useAuth from "../../../hooks/useAuth";
+import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "../../../hooks/useCart";
+import { useNavigate } from "react-router-dom";
 
 const SslPayment = () => {
   const { user } = useAuth();
   const [cart, refetch] = useCart();
+  const axiosSecure = useAxiosSecure();
+  const navigate = useNavigate();
+
   const totalPrice = cart.reduce((total, item) => total + item.price, 0);
 
   const handleCreatePayment = async () => {
@@ -17,6 +23,22 @@ const SslPayment = () => {
       menuItemIds: cart.map((item) => item.menuId),
       status: "pending",
     };
+
+    const res = await axiosSecure.post("/create-ssl-payment", payment);
+    console.log(res.data);
+    refetch();
+
+    if (res.data.payment.insertedId) {
+      // show alert
+      Swal.fire({
+        position: "top-end",
+        icon: "success",
+        title: "Your SSL payment is successful!",
+        showConfirmButton: false,
+        timer: 1500,
+      });
+      navigate("/dashboard/payment-history");
+    }
 
     console.log(payment, "ssl payment");
   };
