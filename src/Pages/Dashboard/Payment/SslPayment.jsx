69a48bc6fb177bd20c3f674e08a480b22ceae773
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-12 20:52:09 +0600

    added a function

--- src/Pages/Dashboard/Payment/SslPayment.jsx
+++ src/Pages/Dashboard/Payment/SslPayment.jsx
@@ -6,14 +6,17 @@ const SslPayment = () => {
   const { cart } = useCart();
   const totalPrice = cart.reduce((total, item) => total + item.price, 0);
 
-  const payment = {
-    email: user?.email,
-    amount: totalPrice,
-    transactionId: "",
-    date: new Date(), // convert utc time use moment js
-    cartIds: cart.map((item) => item._id),
-    menuItemIds: cart.map((item) => item.menuId),
-    status: "pending",
+  const handleCreatePayment = async () => {
+    // now save the payment in database
+    const payment = {
+      email: user?.email,
+      amount: totalPrice,
+      transactionId: "",
+      date: new Date(), // convert utc time use moment js
+      cartIds: cart.map((item) => item._id),
+      menuItemIds: cart.map((item) => item.menuId),
+      status: "pending",
+    };
   };
 
   return <div>SslPayment</div>;
