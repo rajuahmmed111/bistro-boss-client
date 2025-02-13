48ddbed317e3a2d9f11e320bc43771211018e185
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-13 20:42:50 +0600

    update

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -1,8 +1,8 @@
 import { loadStripe } from "@stripe/stripe-js";
 import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { Elements } from "@stripe/react-stripe-js";
-import SslPayment from "./SslPayment";
-// import CheckoutForm from "./CheckoutForm";
+// import SslPayment from "./SslPayment";
+import CheckoutForm from "./CheckoutForm";
 
 // TODO: add publishable key
 const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
@@ -12,8 +12,8 @@ const Payment = () => {
       <SectionTitle subHeading="Please pay bill" heading="Payment" />
       <div>
         <Elements stripe={stripePromise}>
-          {/* <CheckoutForm /> */}
-          <SslPayment />
+          <CheckoutForm />
+          {/* <SslPayment /> */}
         </Elements>
       </div>
     </div>
