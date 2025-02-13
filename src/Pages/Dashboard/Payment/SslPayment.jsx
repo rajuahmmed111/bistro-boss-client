069c4c3715db3cc26412be8bee49dc4edb5d7341
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-13 11:07:17 +0600

    redirect payment gate url

--- src/Pages/Dashboard/Payment/SslPayment.jsx
+++ src/Pages/Dashboard/Payment/SslPayment.jsx
@@ -1,4 +1,4 @@
-import Swal from "sweetalert2";
+// import Swal from "sweetalert2";
 import useAuth from "../../../hooks/useAuth";
 import useAxiosSecure from "../../../hooks/useAxiosSecure";
 import useCart from "../../../hooks/useCart";
@@ -28,22 +28,21 @@ const SslPayment = () => {
     };
 
     const res = await axiosSecure.post("/create-ssl-payment", payment);
-    console.log(res.data);
+    
     refetch();
-
-    if (res.data.payment.insertedId) {
-      // show alert
-      Swal.fire({
-        position: "top-end",
-        icon: "success",
-        title: "Your SSL payment is successful!",
-        showConfirmButton: false,
-        timer: 1500,
-      });
+    if (res.data?.gatewayUrl) {
+      window.location.replace(res.data.gatewayUrl);
+      // Swal.fire({
+      //   position: "top-end",
+      //   icon: "success",
+      //   title: "Your SSL payment is successful!",
+      //   showConfirmButton: false,
+      //   timer: 1500,
+      // });
       navigate("/dashboard/payment-history");
     }
 
-    console.log(payment, "ssl payment", email);
+    // console.log(payment, "ssl payment", email);
   };
 
   return (
