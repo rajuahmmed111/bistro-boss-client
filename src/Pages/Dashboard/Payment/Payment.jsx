402f3617636601cd33952fc6655eb0c36bbec1cb
Raju Ahmmed <ahraju2003@gmail.com>
2025-02-14 21:25:21 +0600

    update stripe publishable key

--- src/Pages/Dashboard/Payment/Payment.jsx
+++ src/Pages/Dashboard/Payment/Payment.jsx
@@ -3,7 +3,7 @@ import SectionTitle from "../../../Components/SectionTitle/SectionTitle";
 import { Elements } from "@stripe/react-stripe-js";
 import CheckoutForm from "./CheckoutForm";
 
-// TODO: add publishable key
+// add publishable key
 const stripePromise = loadStripe(import.meta.env.VITE_PAYMENT_GATEWAY_PK);
 const Payment = () => {
   return (
